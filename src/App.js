c03994469092978df6503033fde3f820ba358d1a
Dmitry Chervochkin <chervochkin2001@mail.ru>
2023-12-04 21:36:16 +0300

    yup

--- src/App.js
+++ src/App.js
@@ -1,112 +1,76 @@
 import styles from './App.module.css';
-import { useState, useRef} from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const sendFormData = (formData) => {
-	console.log(formData);
-};
+const fieldsScheme = yup.object()
+	.shape({
+		email: yup.string()
+			.matches(/^[\w_@.]*$/, 'Неверный E-mail. Допустимые символы: буквы, цифры, "@", точка и нижнее подчёркивание.')
+			.max(30, 'Неверный E-mail. Должно быть не больше 30 символов'),
+		password: yup.string()
+			.matches(/^[\w_]*$/, 'Неверный пароль. Допустимые символы: буквы, цифры, "@", точка и нижнее подчёркивание.')
+			.max(20, 'Неверный пароль. Должно быть не больше 30 символов'),
+		password2: yup.string()
+			.matches(/^[\w_]*$/, 'Неверный пароль. Допустимые символы: буквы, цифры, "@", точка и нижнее подчёркивание.')
+			.max(20, 'Неверный пароль. Должно быть не больше 30 символов'),
+	})
 
 export function App() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [password2, setPassword2] = useState('');
-	const [emailError, setEmailError] = useState(null);
-	const [passError, setPassError] = useState(null);
-	const [passError2, setPassError2] = useState(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			password2: '',
+		},
+		resolver: yupResolver(fieldsScheme),
+	});
 
-	const submitButtonRef = useRef(null);
+	const emailError = errors.email?.message;
+	const passError = errors.password?.message;
+	const passError2 = errors.password2?.message;
 
-	const onEmailChange = ({target}) => {
-		setEmail(target.value);
-		let newError = null;
-
-		if (!/^[\w_@.]*$/.test(target.value)) {
-            newError = 'Неверный E-mail. Допустимые символы: буквы, цифры, "@", точка и нижнее подчёркивание.';
-        } else if (target.value.length > 30) {
-            newError = 'Неверный E-mail. Должно быть не больше 30 символов';
-        }
-        setEmailError(newError);
-	};
-
-	const onPassChange = ({target}) => {
-		setPassword(target.value);
-		let newError = null;
-
-		if (!/^[\w_]*$/.test(target.value)) {
-            newError = 'Неверный пароль. Допустимые символы: буквы, цифры, нижнее подчёркивание';
-        } else if (target.value.length > 20) {
-            newError = 'Неверный E-mail. Должно быть не больше 20 символов';
-        }
-        setPassError(newError);
-	};
-
-	const onPassChange2 = ({target}) => {
-		setPassword2(target.value);
-		let newError = null;
-
-		if (!/^[\w_]*$/.test(target.value)) {
-            newError = 'Неверный пароль. Допустимые символы: буквы, цифры, нижнее подчёркивание';
-        } else if (target.value.length > 20) {
-            newError = 'Неверный E-mail. Должно быть не больше 20 символов';
-        }
-        setPassError2(newError);
-	};
-
-	const onPassBlur = ({ target }) => {
-        if (password !== password2){
-			setPassError2('Пароли не совпадают. Повторите ввод.')
+	function onSubmit(formData){
+		if(formData.password === formData.password2){
+			console.log(formData)
 		}
-    };
-
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if(email === ''){
-			setEmailError('Введите E-mail')
-		}
-		if(password === ''){
-			setPassError('Введите пароль')
-		}
-		if(password2 === ''){
-			setPassError2('Повторите пароль')
-		};
-		sendFormData({ email, password, password2 });
-};
+	};
 
 	return (
 		<div className={styles.App}>
-			<form onSubmit={onSubmit} className={styles.form}>
+			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
 				<div>E-mail адрес*</div>
 				<input
 					name="email"
-					type="email"
-					value={email}
+					type="text"
 					placeholder="Введите E-mail адрес"
-					onChange={onEmailChange}
+					{...register('email')}
 				/>
 				{emailError && <div className={styles.validation}>{emailError}</div>}
 				<div>Придумайте пароль*</div>
 				<input
 					name="password"
 					type="password"
-					value={password}
 					placeholder="Введите пароль"
-					onChange={onPassChange}
+					{...register('password')}
 				/>
 				{passError && <div className={styles.validation}>{passError}</div>}
 				<div>Повторите пароль*</div>
 				<input
 					name="password"
 					type="password"
-					value={password2}
 					placeholder="Введите пароль"
-					onChange={onPassChange2}
-					onBlur={onPassBlur}
+					{...register('password2')}
 				/>
 				{passError2 && <div className={styles.validation}>{passError2}</div>}
 				<button
-					ref={submitButtonRef}
 					className={styles.btn}
 					type="submit"
-					disabled={emailError !== null || passError !== null || passError2 !== null || email === '' || password === '' || password2 === ''}
+					disabled={!!emailError || !!passError || !!passError2}
 				>
 					Зарегистрироваться
 				</button>
